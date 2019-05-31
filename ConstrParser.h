56cf6aebfc046c966437755181b59bc895bb7fa3
Frank Conlon <avanola@gmail.com>
2019-05-31 10:37:06 -0500

    error checking finished. Still need to fix build_asp() for term modification

--- ConstrParser.h
+++ ConstrParser.h
@@ -5,34 +5,34 @@
 #include "AST.h"
 
 class ConstrParser {
-    AST ast;
-    int lineNum, colNum, opcount;
-    std::ifstream *inFile;
-    std::string inFileName, curr_constr, errors;
-    bool start_new_terms_list, valid_program;
-    char next_token();
-    void return_token();
-    std::string get_name();
-    void clear_space();
-    void add_error(std::string);
-    bool is_keyword(std::string);
-    void constraint();
-    bool expr();
-    bool terms_list();
-    bool terms();
-    void term();
-    void varlist();
-    void compop();
-  public:
-    ConstrParser();
-    ConstrParser(std::string);
-    ~ConstrParser();
-    std::string get_inFileName(){ return this->inFileName; }
-    void set_inFile(std::string fn);
-    void parse();
-    void build_asp();
-    std::string get_asp();
-    bool valid(){ return this->valid_program; }
-    std::string get_errors(){ return this->errors; }
+	AST ast;
+	int lineNum, colNum, opcount, errcount;
+	std::ifstream *inFile;
+	std::string inFileName, curr_constr, errors;
+	bool start_new_terms_list, valid_program;
+	char next_token();
+	void return_token();
+	std::string get_name();
+	void clear_space();
+	void add_error(std::string);
+	bool is_keyword(std::string);
+	void constraint();
+	void expr();
+	void terms_list();
+	void terms();
+	void term();
+	void varlist();
+	void compop();
+public:
+	ConstrParser();
+	ConstrParser(std::string);
+	~ConstrParser();
+	std::string get_inFileName() { return this->inFileName; }
+	void set_inFile(std::string fn);
+	void parse();
+	void build_asp();
+	std::string get_asp();
+	bool valid() { return this->valid_program; }
+	std::string get_errors() { return this->errors; }
 };
 #endif
--- test.cpp
+++ test.cpp
@@ -2,38 +2,36 @@
 #include "AST.h"
 #include <iostream>
 
-int main(){
-  ConstrParser *myParser = new ConstrParser("test.txt");
-  myParser->parse();
-  if(myParser->valid()){
-    std::cout << "pass\n";
-    myParser->build_asp();
-    std::cout << myParser->get_asp();
-  }
-  else{
-    std::cout << myParser->get_errors();
-  }
-  /*
-    std::ifstream teststream("test.txt", std::ifstream::in);
-    std::string teststring;
-    teststream >> teststring;
-    std::cout << teststring << std::endl;
-    //teststream.unget();
-    teststream.seekg(-3, std::ios_base::cur);
-    teststream >> teststring;
-    std::cout << teststring << std::endl;
+int main() {
+	ConstrParser *myParser = new ConstrParser("test.txt");
+	myParser->parse();
+	if (myParser->valid()) {
+		std::cout << "pass\n";
+		myParser->build_asp();
+		std::cout << myParser->get_asp();
+	}
+	else {
+		std::cout << myParser->get_errors();
+	}
+	/*
+	std::ifstream teststream("test.txt", std::ifstream::in);
+	std::string teststring;
+	teststream >> teststring;
+	std::cout << teststring << std::endl;
+	//teststream.unget();
+	teststream.seekg(-3, std::ios_base::cur);
+	teststream >> teststring;
+	std::cout << teststring << std::endl;
+	char c;
+	do {
+	c = teststream.get();
+	std::cout << c << std::endl;
+	}while(c >= 0);
+	//myParser->set_inFile("test.txt");
+	//std::cout << "The file is: " << myParser->get_inFile() << std::endl;
+	teststream.close();
+	*/
+	delete myParser;
 
-    char c;
-    do {
-      c = teststream.get();
-      std::cout << c << std::endl;
-    }while(c >= 0);
-
-    //myParser->set_inFile("test.txt");
-    //std::cout << "The file is: " << myParser->get_inFile() << std::endl;
-    teststream.close();
-     */
-    delete myParser;
-
-    return 0;
+	return 0;
 }
